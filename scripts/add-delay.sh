6943579ece2064320af1a6bda52f745dfe4da0de
Victor Osorio <victor.osorio@openet.com>
2024-10-03 17:24:25 -0300

    Fix pumba script

--- scripts/add-delay.sh
+++ scripts/add-delay.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-    docker run -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba pumba netem -d 25m delay --time 300 re2:^kafka
+    docker run -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba netem -d 25m delay --time 300 re2:^kafka

6025b7efccb5e32e3032683381fa70d33631d930
Victor Osorio <victor.osorio@openet.com>
2024-10-03 17:40:42 -0300

    Pumba

--- scripts/add-delay.sh
+++ scripts/add-delay.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+docker exec kafka-0 apk add iproute2-tc
+docker exec kafka-1 apk add iproute2-tc
+docker exec kafka-2 apk add iproute2-tc
 docker run --privileged -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba netem -d 25m delay --time 300 re2:^kafka
